9c66e5c54e1ac9934d38af0a3b730969876a805d
JavoughnP504 <javoughnperkin@gmail.com>
2020-09-26 00:36:21 -0500

    Revert "Added Backgrounf for Front End"
    This reverts commit 6cf1ded3a05b305ab8b876b58d183d00e006627a.

--- Runtime Terror/Runtime Terror/clientapp/src/App.js
+++ Runtime Terror/Runtime Terror/clientapp/src/App.js
@@ -1,30 +1,85 @@
-import React from 'react';
+import React,{useState} from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Bar from './Navbar.js';
-import MyCarousel from './MyCarousel.js';
-
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link, BrowserRouter
-} from "react-router-dom";
-
-
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
 
 function App() {
   return (
-<div className="App">
- <BrowserRouter>
-     <Bar/>
-     <Switch>
-       <Route exact path = "/" component = {MyCarousel}/>
-     </Switch>
-     </BrowserRouter>
-    </div >
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-export default App;
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
 
 
