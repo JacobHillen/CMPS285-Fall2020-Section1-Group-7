f78aa452c353a3b750560339e8d6b3ea2209103f
JavoughnP504 <javoughnperkin@gmail.com>
2020-09-28 17:39:18 -0500

    added images to front end

---
+++ RuntimeTerror/my-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Bar from './Navbar.js';
+import MyCarousel from './MyCarousel.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link, BrowserRouter
+} from "react-router-dom";
+import { Button, Navbar } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">   
+    <BrowserRouter>
+     <Bar/>
+     <Switch>
+       <Route exact path = "/" component = {MyCarousel}/>
+     </Switch>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
